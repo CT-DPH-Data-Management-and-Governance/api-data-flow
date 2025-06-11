3722b26370bc8ca443e76f090a97501a0309ad6f
asenetcky <me@asenetcky.dev>
2025-06-11 12:36:30 -0400

    feat(api): add function placeholders

---
+++ src/api/acs.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from polars import LazyFrame
+# import requests
+
+# TODO: consider using pydantic and a target class
+
+def grab_file_targets(file_path: Path | None) -> str:
+    return None
+
+def grab_data(api: str | None) -> LazyFrame:
+    return None
