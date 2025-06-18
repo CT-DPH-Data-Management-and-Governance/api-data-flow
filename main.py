ca194697f50c0b10bc4314f03a89c478dcabd6c4
Alexander Senetcky <alexander.senetcky@ct.gov>
2025-06-18 15:59:21 -0400

    Feat: Implement Dataops python package (#7)
    * feat: add latest dataops
    
    * feat: add wip python
    
    * feat(r): add some variables
    
    * feat(deps): update uv.lock
    
    * tidy: remove all the old scripts replaced by dataops
    
    * feat: add main.py to download apis to parquet
    
    * fix: specify types

---
+++ main.py
@@ -0,0 +1,35 @@
+from dataops.models import CensusAPIEndpoint
+import polars as pl
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CENSUS_API_KEY = os.getenv("CENSUS_API_KEY")
+
+
+def fetch_data(urls: list[str]) -> pl.LazyFrame:
+    all_frames = []
+
+    for url in urls:
+        df = CensusAPIEndpoint.from_url(url).fetch_tidy_data().lazy()
+        print(df.head())
+        all_frames.append(df)
+
+    return pl.concat(all_frames)
+
+
+def local_urls() -> list[str]:
+    path = Path.cwd() / "data" / "raw" / "census-api.csv"
+    return pl.read_csv(path).to_series().to_list()
+
+
+def main():
+    print("pulling from census apis")
+    lf = fetch_data(local_urls())
+    lf.sink_parquet("whole-game.parquet")
+
+
+if __name__ == "__main__":
+    main()
