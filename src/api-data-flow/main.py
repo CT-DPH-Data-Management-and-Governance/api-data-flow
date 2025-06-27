09385cd3d7d3ee2e819befecb9d1fd79597eb8a4
asenetcky <me@asenetcky.dev>
2025-06-27 08:10:25 -0400

    feat: add update_source()

--- src/api-data-flow/main.py
+++ src/api-data-flow/main.py
@@ -101,8 +101,11 @@ def only_new(cutoff: dt.date = CUTOFF):
     return new
 
 
-def update_source():
-    None
+def update_source(source: pl.DataFrame):
+    new = source.with_columns(pl.lit(TODAY).alias("date_last_pulled")).to_dicts()
+
+    with Socrata(DOMAIN, TOKEN, USERNAME, PASSWORD) as client:
+        client.upsert(TABLE_SOURCE, new)
 
 
 def main():
