e0b9cbc04ab6db9fcded4723d5fcc43fe0ba2c9e
asenetcky <me@asenetcky.dev>
2025-07-11 07:18:11 -0400

    feat: refine etl names and setting use

--- src/api-data-flow/etl.py
+++ src/api-data-flow/etl.py
@@ -1,19 +1,57 @@
 from sodapy import Socrata
-from fetch import fetch_source
-
+from dataops.portal import fetch_data
+from dataops.models import ApplicationSettings
+import datetime as dt
 import polars as pl
 
 
-def only_new(cutoff: dt.date = CUTOFF):
-    df = fetch_source()
-    new = df.with_columns(pl.col("date_last_pulled").str.to_datetime()).filter(
-        (pl.col("date_last_pulled").le(cutoff)) & (pl.col("active").eq("1"))
+def needs_refresh(
+    source: str | None = None,
+    refresh: str = "1y",
+    settings: ApplicationSettings | None = None,
+) -> pl.LazyFrame:
+    """
+    Return a LazyFrame of only new endpoints or ones needing
+    a data update according to the refresh variable.
+
+    resfresh uses the polars and datetime offset_by calendar
+    string notation.
+    """
+
+    df = fetch_data(source=source, settings=settings)
+    today = dt.datetime.today()
+    new = (
+        df.with_columns(pl.col("date_last_pulled").str.to_datetime())
+        .with_columns(pl.col("date_last_pulled").dt.offset_by(refresh).alias("refresh"))
+        .filter((pl.col("refresh").le(pl.lit(today))) & (pl.col("active").eq("1")))
+        .drop(pl.col(refresh))
     )
     return new
 
 
-def update_source(source: pl.DataFrame):
-    new = source.with_columns(pl.lit(TODAY).alias("date_last_pulled")).to_dicts()
+def update_source(
+    data: pl.DataFrame | pl.LazyFrame,
+    source: str | None = None,
+    settings: ApplicationSettings | None = None,
+) -> pl.LazyFrame:
+    """Update the endpoint source date last pulled based on row id."""
+    if settings is None:
+        settings = ApplicationSettings()
 
-    with Socrata(DOMAIN, TOKEN, USERNAME, PASSWORD) as client:
-        client.upsert(TABLE_SOURCE, new)
+    if source is None:
+        source = settings.source_id
+
+    now = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    new = data.lazy().with_columns(pl.lit(now).alias("date_last_pulled"))
+    dict_new = new.collect().to_dicts()
+
+    with Socrata(
+        settings.domain,
+        settings.socrata_token,
+        settings.socrata_user,
+        settings.socrata_pass,
+    ) as client:
+        client.upsert(source, dict_new)
+
+    return new
