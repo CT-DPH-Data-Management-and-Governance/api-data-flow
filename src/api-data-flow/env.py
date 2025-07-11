f3d9224d84406dad18c6201773963e48e106ba8d
asenetcky <me@asenetcky.dev>
2025-07-10 21:33:14 -0400

    feat: swap dotenv for pydantic basesettings

--- src/api-data-flow/env.py
+++ src/api-data-flow/env.py
@@ -1,28 +1,11 @@
 from dataops.models import ApplicationSettings
-from dotenv import load_dotenv
-import os
-import sys
 import logging
 
 
 # TODO add some more utitlity around this
 def load_settings():
+    logging.info("Attempting to load environmental variables.")
     settings = ApplicationSettings()
+    logging.info("Environmental variables successfully loaded.")
 
     return settings
-
-
-# environmental variables/secrets
-logging.info("Attempting to load environmental variables.")
-if load_dotenv():
-    CENSUS_API_KEY = os.getenv("CENSUS_API_KEY")
-    TABLE_SOURCE = os.getenv("TABLE_SOURCE")
-    TABLE_TARGET = os.getenv("TABLE_TARGET")
-    USERNAME = os.getenv("USERNAME")
-    PASSWORD = os.getenv("PASSWORD")
-    TOKEN = os.getenv("TOKEN")
-    DOMAIN = os.getenv("DOMAIN")
-    logging.info("Environmental variables successfully loaded.")
-else:
-    logging.critical("Environmental variables cannot be loaded - exiting process.")
-    sys.exit(1)
